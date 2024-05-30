17bb6b3cc483b246e45470610f6e76a209a01808
Fadhli270703 <mohfadli095@gmail.com>
2024-05-30 23:17:50 +0700

    rubah databse

--- ResizenCompress/register.php
+++ ResizenCompress/register.php
@@ -9,13 +9,13 @@ if(isset($_POST['signUp'])){
     $password=$_POST['password'];
     $password=md5($password);
 
-     $checkEmail="SELECT * From users where email='$email'";
+     $checkEmail="SELECT * From loginuser where email='$email'";
      $result=$conn->query($checkEmail);
      if($result->num_rows>0){
         echo "Email Address Already Exists !";
      }
      else{
-        $insertQuery="INSERT INTO users(firstName,lastName,email,password)
+        $insertQuery="INSERT INTO loginuser(firstName,lastName,email,password)
                        VALUES ('$firstName','$lastName','$email','$password')";
             if($conn->query($insertQuery)==TRUE){
                 header("location: index.php");
@@ -33,7 +33,7 @@ if(isset($_POST['signIn'])){
    $password=$_POST['password'];
    $password=md5($password) ;
    
-   $sql="SELECT * FROM users WHERE email='$email' and password='$password'";
+   $sql="SELECT * FROM loginuser WHERE email='$email' and password='$password'";
    $result=$conn->query($sql);
    if($result->num_rows>0){
     session_start();
